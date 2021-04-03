e8938a06af1745a1c677cbed465e500d8e3d97cd
Shivesh A Pathak <shiveshpathak1@MacBook-Pro.local>
2021-04-03 12:00:09 -0700

    .

---
+++ fits/fit_graphene.py
@@ -0,0 +1,27 @@
+import h5py
+import numpy as np 
+import statsmodels.api as sm
+from training_data import graphene_training_data
+
+# Single-layer graphene fits
+df1, df2, df3 = graphene_training_data('../datasets/graphene/')
+fits = {
+    't01': {
+        'df': df1,
+    },
+    't02': {
+        'df': df2,
+    },
+    't03': {
+        'df': df3,
+    },
+}
+f = h5py.File('fit_graphene.hdf5','w')
+for k in fits.keys():
+    y = fits[k]['df']['t']
+    X = sm.add_constant(fits[k]['df'].drop(['t'], axis = 1))
+    lm = sm.OLS(y, X).fit()
+    g = f.create_group(k)
+    g.create_dataset('parameters', data=lm.params)
+    g.create_dataset('standard_errors', data=lm.HC0_se)
+f.close() 
