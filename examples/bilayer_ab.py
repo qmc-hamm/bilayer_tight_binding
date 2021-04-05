43cc73bf36e7b4cebf62918163dde1ba40a34610
Shivesh A Pathak <shiveshpathak1@MacBook-Pro.local>
2021-04-05 15:55:21 -0700

    updated examples

---
+++ examples/bilayer_ab.py
@@ -0,0 +1,36 @@
+import json
+from pythtb import *
+import matplotlib.pyplot as plt
+from bilayer_tight_binding.model import bilayer
+
+# use model to compute hoppings
+R0 = 2.6834 # Bohr
+lattice_vectors = np.array([[np.sqrt(3) * R0, 0.0, 0.0], [-np.sqrt(3)/2 * R0, 3/2 * R0, 0.0]])
+atomic_basis = np.array([[0, 0, 0], [0, R0, 0], [0, R0, 6.497], [0, 2 * R0, 6.497]])
+i =  [0, 1, 1, 2, 3, 3, 1, 1,  1,  1, 0,  0,  0,  0,  0,  0]
+j =  [1, 0, 0, 3, 2, 2, 2, 3,  3,  3, 2,  2,  3,  2,  3,  3]
+di = [0, 0, 1, 0, 0, 1, 0, 0,  0, -1, 0,  0,  0, -1, -1, -1]
+dj = [0, 1, 1, 0, 1, 1, 0, 0, -1, -1, 0, -1, -1, -1, -1, -2]
+hoppings = bilayer(lattice_vectors, atomic_basis, i, j, di, dj)
+
+# compute bands
+lat = lattice_vectors[:, :-1]
+orb = atomic_basis[:, :-1]
+gra = tb_model(2, 2, lat, orb)
+k=[[0.5, 0.0], [2/3., -1./3.], [0.5, 0.]]
+for ii, jj, dii, djj, hopping in zip(i, j, di, dj, hoppings):
+    gra.set_hop(hopping, ii, jj, [dii, djj])
+(k_vec, k_dist, k_node) = gra.k_path(k, 1000)
+evals = gra.solve_all(k_vec)
+evals -= min(evals[2, :]) 
+
+# plot the band structure
+fig, ax = plt.subplots(figsize = (3, 3)) 
+for i in range(len(evals_aa)):
+    ax.plot(k_dist_aa, evals_aa[i,:], 'k-')
+ax.set_ylabel(r'$E - E_F$ (eV)')
+ax.set_ylim((-5, 5)) 
+ax.set_xticks(k_node)
+ax.set_xticklabels(["M", "K", "M"])
+ax.set_xlim(k_node[0], k_node[-1])
+fig.savefig("bilayer_ab.pdf", bbox_inches='tight')
--- examples/graphene.py
+++ examples/graphene.py
@@ -1,11 +1,10 @@
 import sys
 import json
-sys.path.append('../')
-from model import graphene 
 from pythtb import *
 import matplotlib.pyplot as plt
+from bilayer_tight_binding.model import graphene 
 
-# Use our model to compute hoppings
+# Use our model to compute hoppings up to 3NN
 R0 = 2.6834 # Bohr
 lattice_vectors = np.array([[np.sqrt(3) * R0, 0.0, 0.0], [-np.sqrt(3)/2 * R0, 3/2 * R0, 0.0]])
 atomic_basis    = np.array([[0, 0, 0], [0, R0, 0]])
@@ -15,25 +14,21 @@ di = [0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, -1]
 dj = [0, 1, 1, 0, 1, 1, 0, 1, 1, 2, 0,  0]
 hoppings = graphene(lattice_vectors, atomic_basis, i, j, di, dj)
 
-# compute and plot the band structure
-z = 0
+# compute the band structure
 lat = lattice_vectors[:, :-1]
 orb = atomic_basis[:, :-1]
 gra = tb_model(2, 2, lat, orb)
 k=[[0.0, 0.0], [2/3., -1./3.], [1./3., 1./3.]]
-
-color = {3:'b', 9:'r', 12:'g'}
-label = {3:r'$t_{01}$', 9:r'$t_{01}, t_{02}$', 12:'$t_{01}, t_{02}, t_{03}$'}
-fig, ax = plt.subplots(figsize = (3,3))
 for ii, jj, dii, djj, hopping in zip(i, j, di, dj, hoppings):
-    z += 1
     gra.set_hop(hopping, ii, jj, [dii, djj])
-    if (z==3) or (z==9) or (z==12):
-        (k_vec, k_dist, k_node) = gra.k_path(k, 100)
-        evals = gra.solve_all(k_vec)
-        evals -= min(evals[1, :])
-        ax.plot(k_dist, evals[0,:], color=color[z], lw = 3, alpha = 0.5)
-        ax.plot(k_dist, evals[1,:], color=color[z], label=label[z], lw = 3, alpha = 0.5)
+(k_vec, k_dist, k_node) = gra.k_path(k, 100)
+evals = gra.solve_all(k_vec)
+evals -= min(evals[1, :])
+
+# plot band structure
+fig, ax = plt.subplots(figsize = (3,3))
+ax.plot(k_dist, evals[0,:], color='b', lw = 3, alpha = 0.5)
+ax.plot(k_dist, evals[1,:], color='b', label=label[z], lw = 3, alpha = 0.5)
 
 # plot the reference data
 ref = json.load(open('graphene.json','r'))
