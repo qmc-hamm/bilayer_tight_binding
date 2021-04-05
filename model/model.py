7e4b842d00980b00867d72c1c86067372da564c7
Shivesh A Pathak <shiveshpathak1@MacBook-Pro.local>
2021-04-05 09:44:50 -0700

    model written up

--- model/model.py
+++ model/model.py
@@ -1,8 +1,9 @@
 import sys
 import numpy as np 
 import pandas as pd
+sys.path.apend('../fits')
 sys.path.append('../descriptors')
-
+from functions import fang
 import descriptors_graphene, descriptors_bilayer
 
 def load_graphene_fit():
@@ -17,7 +18,10 @@ def load_graphene_fit():
     return fit
 
 def load_bilayer_fit():
-    return
+    fit = {}
+    with h5py.File('../fits/fit_bilayer.hdf5','w') as hdf:
+        fit['fang'] = list(hdf['fang']['parameters'])
+    return fit
 
 def graphene(lattice_vectors, atomic_basis, i, j, di, dj):
     """
@@ -40,9 +44,9 @@ def graphene(lattice_vectors, atomic_basis, i, j, di, dj):
     fit = load_graphene_fit()
 
     # Predict hoppings
-    t01 = np.dot(fit[0][1:], descriptors) + fit[0][0]
-    t02 = np.dot(fit[1][1:], descriptors) + fit[1][0]
-    t03 = np.dot(fit[2][1:], descriptors) + fit[2][0]
+    t01 = np.dot(fit['t01'][1:], descriptors) + fit['t01'][0]
+    t02 = np.dot(fit['t02'][1:], descriptors) + fit['t02'][0]
+    t03 = np.dot(fit['t03'][1:], descriptors) + fit['t03'][0]
 
     # Reorganize
     hoppings = np.zeros(len(i))
@@ -52,3 +56,36 @@ def graphene(lattice_vectors, atomic_basis, i, j, di, dj):
 
     return hoppings
 
+def bilayer(lattice_vectors, atomic_basis, i, j, di, dj):
+    """
+    Input: 
+        lattice_vectors - float (nlat x 3) where nlat = 2 lattice vectors for graphene
+        atomic_basis    - float (natoms x 3) where natoms are the number of atoms in the computational cell 
+        i, j            - int   (n) list of atomic bases you are hopping between
+        di, dj          - int   (n) list of displacement indices for the hopping
+    Output:
+        hoppings        - float (n) list of hoppings for the given i, j, di, dj
+    """
+    # Extend lattice_vectors to (3 x 3) for our descriptors, the third lattice vector is arbitrary
+    latt_vecs = np.append(lattice_vectors, [[0, 0, 25]], axis = 0)
+
+    # Get the bi-layer descriptors 
+    descriptors = descriptors_bilayer.descriptors(lattice_vectors, atomic_basis, di, dj, i, j)
+    
+    # Partition the intra- and inter-layer hoppings indices 
+    interlayer = np.array(descriptors['dz'] > 0)
+    
+    # Compute the inter-layer hoppings
+    fit = load_bilayer_fit()
+    X = descriptors[['dxy','theta_12','theta_21']].values[interlayer]
+    interlayer_hoppings = fang(X, *fit['fang'])
+
+    # Compute the intra-layer hoppings
+    intralayer_hoppings = graphene(lattice_vectors, atomic_basis, i[~interlayer], j[~interlayer], di[~interlayer], dj[~interlayer])
+
+    # Reorganize
+    hoppings = np.zeros(len(i))
+    hoppings[interlayer] = interlayer_hoppings
+    hoppings[~interlayer] = intralayer_hoppings
+
+    return hoppings
