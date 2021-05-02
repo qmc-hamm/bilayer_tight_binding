93298b59521d5d9507eadbb96336801a980b1d4c
Shivesh A Pathak <shiveshpathak1@MacBook-Pro.local>
2021-05-02 09:55:25 -0700

    fit graphene good

--- bilayer_tight_binding/fits/fit_graphene.py
+++ bilayer_tight_binding/fits/fit_graphene.py
@@ -6,17 +6,16 @@ from bilayer_tight_binding.fits.training_data import graphene_training_data
 
 # Single-layer graphene fits
 df1, df2, df3 = graphene_training_data('../../datasets/graphene/')
-df1_, df2_, df3_ = graphene_training_data('../../datasets/bilayer/')
 
 fits = {
     't01': {
-        'df': pd.concat([df1, df1_]),
+        'df': df1,
     },
     't02': {
-        'df': pd.concat([df2, df2_]),
+        'df': df2,
     },
     't03': {
-        'df': pd.concat([df3, df3_]),
+        'df': df3,
     },
 }
 f = h5py.File('fit_graphene.hdf5','w')
@@ -24,7 +23,8 @@ for k in fits.keys():
     y = fits[k]['df']['t']
     X = sm.add_constant(fits[k]['df'].drop(['t'], axis = 1))
     lm = sm.OLS(y, X).fit()
+
     g = f.create_group(k)
     g.create_dataset('parameters', data=lm.params)
     g.create_dataset('standard_errors', data=lm.HC0_se)
-f.close() 
+f.close()
