19021980cae7131e08773caf5d94c2109809cf05
Shivesh A Pathak <shiveshpathak1@MacBook-Pro.local>
2021-09-08 12:51:13 -0600

    added helper and api

---
+++ bilayer_tight_binding/api.py
@@ -0,0 +1,5 @@
+# ASE and Pythtb interface, simple default behavior
+from bilayer_tight_binding.helper import pythtb_model as tb_model
+
+# Separate models for custom behavior
+from bilayer_tight_binding.model import graphene, bilayer, moon_bilayer
