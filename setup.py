62e3b119d81206dfbb7fed8e8c70764f81f4890c
Shivesh A Pathak <shiveshpathak1@MacBook-Pro.local>
2021-09-09 10:54:09 -0600

    .

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
         name='bilayer_letb',
-        version='0.0.1',
+        version='0.0.2',
         author="Shivesh Pathak, Tawfiq Rakib, Run Hou, Andriy Nevidomskyy, Elif Ertekin, Harley T. Johnson, Lucas K. Wagner",
         author_email="shiveshapathak@gmail.com, lkwagner@illinois.edu",
         description="LETB model for twisted bilayer graphene",
+        long_description="LETB model for twisted bilayer graphene",
         url="https://github.com/qmc-hamm/bilayer_tight_binding",
         packages=find_packages(),
         include_package_data=True,
